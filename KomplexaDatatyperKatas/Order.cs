250611361283d1cf9ce469e81e83a54ead37ecee
Urkellitow <Shkelqimcakaj@hotmail.com>
2021-09-01 16:55:49 +0200

    arbetat lite

---
+++ KomplexaDatatyperKatas/Order.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualBasic.CompilerServices;
+
+namespace KomplexaDatatyperKata
+{
+    public class Order
+    {
+        private int _orderNumber = 0;
+        public string Name;
+        private static DateTime _timeOfOrder;
+        private double _totalCost;
+        public static int amountOfOrders;
+
+
+        public Order(string name)
+        {
+            Name = name;
+            amountOfOrders++;
+            Console.WriteLine($"{amountOfOrders} total order(s) made!\n");
+            _timeOfOrder = DateTime.Now;
+            _orderNumber = amountOfOrders;
+        }
+        //public static int GenerateOrderNumber()
+        //{
+        //    int k;
+        //    bool b = true;
+        //    do
+        //    {
+        //        int min = 0000;
+        //        int max = 9999;
+        //        Random orderNumberGenerator = new Random();
+        //        k = orderNumberGenerator.Next(min, max);
+        //        if (!listOfOrderNumbers.Contains(k))
+        //        {
+        //            listOfOrderNumbers.Add(k);
+        //            b = false;
+        //        }
+        //    } while (b);
+
+        //    return k;
+        //}
+
+        //public Order NewOrder(string name)
+        //{
+        //    List<string> listOfOrders = new List<string>();
+        //    int countOfItems = 0;
+        //    while (true)
+        //    {
+        //        Console.Clear();
+
+        //        Console.WriteLine("1: Add an electric bicycle");
+        //        Console.WriteLine("2: Add a trampoline");
+        //        Console.WriteLine("3: Add a bouquet");
+        //        Console.WriteLine("4: Add something else");
+        //        Console.WriteLine("5: Show my order");
+        //        Console.WriteLine("6: Finish my order");
+        //        Console.WriteLine("7: Exit without placing order");
+
+        //        Console.Write("Type option and press enter:");
+        //        int choice = int.Parse(Console.ReadLine());
+
+        //        Console.Clear();
+
+        //        if (choice == 1)
+        //        {
+        //            listOfOrders.Add("electric bycycle");
+        //            countOfItems++;
+        //        }
+        //        else if (choice == 2)
+        //        {
+        //            listOfOrders.Add("trampoline");
+        //            countOfItems++;
+        //        }
+        //        else if (choice == 3)
+        //        {
+        //            listOfOrders.Add("bouquet");
+        //            countOfItems++;
+        //        }
+        //        else if (choice == 4)
+        //        {
+        //            Console.WriteLine("What would u like to order? ");
+        //            Console.WriteLine("(0) to return to menu.");
+        //            var t = Convert.ToString(Console.ReadLine());
+        //            if (t == "0")
+        //            {
+        //                continue;
+        //            }
+        //            else
+        //                listOfOrders.Add(t);
+        //        }
+        //        else if (choice == 5)
+        //        {
+        //            Console.WriteLine($"Total amount of items: {countOfItems}.");
+        //            listOfOrders.ForEach(i => Console.WriteLine(i));
+        //            Console.WriteLine("Press any key to continue.");
+        //            Console.ReadKey();
+        //        }
+        //        else if (choice == 6)
+        //        {
+        //            Console.WriteLine("Are you sure? ");
+        //            var answer = Convert.ToString(Console.ReadLine());
+        //            if (answer.ToLower() == "yes" || answer.ToLower() == "ye" || answer.ToLower() == "y")
+        //            {
+        //                return new Order(listOfOrders, name);
+        //            }
+
+        //        }
+        //        else if (choice == 7)
+        //        {
+        //            Console.WriteLine("Are you sure? ");
+        //            var answer = Convert.ToString(Console.ReadLine());
+        //            if (answer.ToLower() == "yes" || answer.ToLower() == "ye" || answer.ToLower() == "y")
+        //            {
+        //                break;
+        //            }
+        //        }
+
+        //    }
+
+        //    return null;
+        //}
+
+    }
+
+}
+
--- KomplexaDatatyperKatas/Program.cs
+++ KomplexaDatatyperKatas/Program.cs
@@ -1,4 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using KomplexaDatatyperKata;
+using System.Threading;
 
 namespace KomplexaDatatyperKatas
 {
@@ -6,17 +11,22 @@ namespace KomplexaDatatyperKatas
     {
         static void Main(string[] args)
         {
+            List<Order> listOfOrders = new List<Order>();
+            List<String> diffItems = new List<string>();
+            
+
+
             while (true)
             {
-                Console.Clear();
-
-                Console.WriteLine("1: Order an electric bicycle");
-                Console.WriteLine("2: Order a trampoline");
-                Console.WriteLine("3: Order a bouquet");
-                Console.WriteLine("4: Order something else");
-                Console.WriteLine("5: Show all orders");
-                Console.WriteLine("6: Show amount of each order");
-                Console.WriteLine("7: Exit");
+                Console.WriteLine("1: Add an electric bicycle");
+                Console.WriteLine("2: Add a trampoline");
+                Console.WriteLine("3: Add a bouquet");
+                Console.WriteLine("4: Add something else");
+                Console.WriteLine("5: Show orders");
+                Console.WriteLine("6: Print different items that was ordered");
+                Console.WriteLine("7: Exit without placing order");
+                Console.WriteLine("8: Get amount of orders");
+                Console.WriteLine("9: Time wich last order was created:");
 
                 Console.Write("Type option and press enter:");
                 int choice = int.Parse(Console.ReadLine());
@@ -25,17 +35,115 @@ namespace KomplexaDatatyperKatas
 
                 if (choice == 1)
                 {
-
+                    listOfOrders.Add(new Order("electric bicycle"));
+                    if (!diffItems.Contains("electric bicycle"))
+                    {
+                        diffItems.Add("electric bicycle");
+                    }
                 }
                 else if (choice == 2)
                 {
+                    listOfOrders.Add(new Order("trampoline"));
+                    if (!diffItems.Contains("trampoline"))
+                    {
+                        diffItems.Add("trampoline");
+                    }
+                }
+                else if (choice == 3)
+                {
+                    listOfOrders.Add(new Order("bouquet"));
+                    if (!diffItems.Contains("bouquet"))
+                    {
+                        diffItems.Add("bouquet");
+                    }
+                }
+                else if (choice == 4)
+                {
+                    Console.WriteLine("What would u like to order? ");
+                    Console.WriteLine("(0) to return to menu.");
+                    var t = Convert.ToString(Console.ReadLine());
+                    if (t == "0")
+                    {
+                        continue;
+                    }
+                    else
+                        listOfOrders.Add(new Order(t.ToLower()));
+                    if (!diffItems.Contains(t.ToLower()))
+                    {
+                        diffItems.Add(t.ToLower());
+                    }
+                    Console.Clear();
+                }
+                else if (choice == 5)
+                {
+                    Console.WriteLine("List of ordered items: ");
+                   listOfOrders.ForEach(x=> Console.WriteLine(x.Name));
+                   Console.WriteLine();
+                }
+                else if (choice == 6)
+                {
+                    diffItems.ForEach(x => Console.WriteLine(x));
+                    Console.WriteLine();
+                }
+
+                else if (choice == 7)
+                {
+                    Console.WriteLine("Are you sure? ");
+                    var answer = Convert.ToString(Console.ReadLine());
+                    if (answer.ToLower() == "yes" || answer.ToLower() == "ye" || answer.ToLower() == "y")
+                    {
+                        break;
+                    }
+                
+                }
+                else if (choice==8)
+                {
+                    Console.WriteLine(Order.amountOfOrders);
+                    Console.ReadKey();
+                    
+                }
+                else
+                {
+                    Console.WriteLine("Invalid choice, retry.");
 
                 }
-                else if(choice == 7)
-                {
-                    break;
-                }
+
             }
+            //bool b =true;
+            //while (b)
+            //{
+            //    switch (choice)
+            //    {
+            //        case 1:
+            //            listOfOrders.Add(new Order("electric bicycle"));
+            //            break;
+            //        case 2:
+            //            listOfOrders.Add(new Order("trampoline"));
+            //            break;
+            //        case 3:
+            //            listOfOrders.Add(new Order("bouquet"));
+            //            break;
+            //        case 4:
+            //            Console.WriteLine("What would you like to order?");
+            //            var item = Console.ReadLine();
+            //            listOfOrders.Add(new Order(item.ToString()));
+            //            break;
+            //        case 5:
+            //            listOfOrders.ForEach(x => Console.WriteLine(x));
+            //            break;
+            //        case 6:
+            //            Console.WriteLine("??????");
+            //            break;
+            //        case 7:
+            //            b = false;
+            //            break;
+            //        default:
+            //            Console.WriteLine("Cannot understand ur choice");
+            //            break;
+
+            //    }
+            //}
         }
+
     }
 }
